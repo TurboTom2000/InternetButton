32a4a5ee2c2ec51682c2accee6c4cf046d6a365c
TurboTom2000 <thmas.fasswald@edu.campus02.at>
2016-02-06 12:57:49 +0100

    t1

--- swe-iot/src/main/java/org/c02/iot/InternetButtonImpl.java
+++ swe-iot/src/main/java/org/c02/iot/InternetButtonImpl.java
@@ -24,11 +24,7 @@ public class InternetButtonImpl implements InternetButtonApi {
 			b2=b1%11;
 			
 			System.out.println("b1=" + b1 + " b2= " + b2);
-			
-			//System.out.println("Button 1: " + wrapper.readVariable("countButton1"));
-//			System.out.println("Button 2: " + wrapper.readVariable("countButton2"));
-//			System.out.println("Button 3: " + wrapper.readVariable("countButton3"));
-//			System.out.println("Button 4: " + wrapper.readVariable("countButton4"));
+
 		} catch (IOException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
@@ -46,10 +42,7 @@ public class InternetButtonImpl implements InternetButtonApi {
 			
 			System.out.println("b1=" + b1 + " b2= " + b2);
 			
-			//System.out.println("Button 1: " + wrapper.readVariable("countButton1"));
-//			System.out.println("Button 2: " + wrapper.readVariable("countButton2"));
-//			System.out.println("Button 3: " + wrapper.readVariable("countButton3"));
-//			System.out.println("Button 4: " + wrapper.readVariable("countButton4"));
+
 		} catch (IOException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
@@ -57,15 +50,12 @@ public class InternetButtonImpl implements InternetButtonApi {
 		return b2;
 	}
 
-	public void setLed(int postition, Color color) {
-		
-		
+	public void setLed(int postition, Color color) {		
 		
 		String blue = String.format("%03d", color.getBlue());
 		String green = String.format("%03d", color.getGreen());
 		String red = String.format("%03d", color.getRed());
-	    String posit = String.format("%02d", postition);
-		
+	    String posit = String.format("%02d", postition);		
 		
 		String positionColor = ""+posit+red+green+blue;
 		
