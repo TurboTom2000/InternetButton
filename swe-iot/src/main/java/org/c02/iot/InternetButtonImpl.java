430a90f11d8fe9595a4fddff19ebff7c4dc62461
Markus Singer <markus.singer@edu.campus02.at>
2016-02-06 11:00:55 +0100

    testen für generellen ablauf

--- swe-iot/src/main/java/org/c02/iot/InternetButtonImpl.java
+++ swe-iot/src/main/java/org/c02/iot/InternetButtonImpl.java
@@ -20,12 +20,50 @@ public class InternetButtonImpl implements InternetButtonApi {
 	}
 
 	public void setLed(int postition, Color color) {
-		// TODO Auto-generated method stub
+		
+		try {
+			wrapper.callMethod("led","01255255255");
+		} catch (ParticleException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			wrapper.callMethod("led","05255000000");
+		} catch (ParticleException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			wrapper.callMethod("led","06000255000");
+		} catch (ParticleException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			wrapper.callMethod("led","07000000255");
+		} catch (ParticleException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			wrapper.callMethod("led","11000255255");
+		} catch (ParticleException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	
 	}
 
 	public void allLedsOff() {
-		// TODO Auto-generated method stub
-
+		
+		try {
+			wrapper.callMethod("ledsOff",null);
+		} catch (ParticleException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+		
 	}
 
 	public void playSound() 
